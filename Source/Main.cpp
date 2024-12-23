f4703401a29434051b67bbd1069c578cfef43ac9
aike <aaiikkee@gmail.com>
2024-12-23 17:30:08 +0900

    refactoring

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -22,6 +22,8 @@ bool isInt128(std::string s);
 bool isInt256(std::string s);
 int getNumber(std::string s);
 
+std::string toString(int byte0, int byte1, int byte2);
+
 
 //==============================================================================
 int main (int argc, char* argv[])
@@ -54,13 +56,13 @@ int main (int argc, char* argv[])
     }
     else if ((arg[1] == "help") && (arg[2] == ""))
     {
-        listDevices();
-        return 0;
+        usage();
     }
 
     int device = 0;
     int ch = 0;
     std::string cmd;
+    int byte0 = 0;
     int byte1 = 0;
     int byte2 = 0;
 
@@ -70,26 +72,15 @@ int main (int argc, char* argv[])
         device = std::stoi(arg[1]);
         ch = std::stoi(arg[2]);
         cmd = toLower(arg[4]);
-        byte2 = getNumber(arg[5]);
+        int n = getNumber(arg[5]);
+        byte2 = (n < 0) ? 0 : n;
 
-        if (cmd == "allsoundoff")
-        {
-            int code = 120;
-            sendMessage(device, ch, 0xB0 + ch, code, byte2);
-            std::cout << "  AllSoundOff";
-            std::cout << " " << code << " " << byte2;
-        }
-        else if (cmd == "allnotesoff")
-        {
-            int code = 123;
-            sendMessage(device, ch, 0xB0 + ch, code, byte2);
-            std::cout << "  AllSoundOff";
-            std::cout << " " << code << " " << byte2;
-        }
+        if      (cmd == "allsoundoff") { sendMessage(device, ch, 0xB0 + ch, 120, byte2); }
+        else if (cmd == "allnotesoff") { sendMessage(device, ch, 0xB0 + ch, 123, byte2); }
     }
 
     ////// (4) mider device port command byte1 byte2 ////////
-    if (isInt128(arg[1]) && isInt128(arg[2]) && isAlphabet(arg[3]))
+    else if (isInt128(arg[1]) && isInt128(arg[2]) && isAlphabet(arg[3]))
     {
         device = std::stoi(arg[1]);
         ch = std::stoi(arg[2]);
@@ -97,19 +88,22 @@ int main (int argc, char* argv[])
         byte1 = getNumber(arg[4]);
         byte2 = getNumber(arg[5]);
 
-        if (cmd == "noteon")
-        {
-            sendMessage(device, ch, 0x90 + ch, byte1, byte2);
-            std::cout << "  NoteOn";
-            std::cout << " " << byte1 << " " << byte2;
-        }
-        else if (cmd == "noteoff")
-        {
-            sendMessage(device, ch, 0x80 + ch, byte1, byte2);
-            std::cout << "  NoteOff";
-            std::cout << " " << byte1 << " " << byte2;
-        }
+        if      (cmd == "noteon")  { sendMessage(device, ch, 0x90 + ch, byte1, byte2); }
+        else if (cmd == "noteoff") { sendMessage(device, ch, 0x80 + ch, byte1, byte2); }
     }
+
+    ////// (5) mider device byte0 byte1 byte2 ////////
+    else if (isInt128(arg[1]) && isInt128(arg[2]) && isInt256(arg[3]) && isInt256(arg[4]) && isInt256(arg[5]))
+    {
+        device = std::stoi(arg[1]);
+        ch    = getNumber(arg[3]) & 0x0F;
+        byte0 = getNumber(arg[3]);
+        byte1 = getNumber(arg[4]);
+        byte2 = getNumber(arg[5]);
+
+        sendMessage(device, ch, 0x90 + ch, byte1, byte2);
+    }
+
 
     std::cout << std::endl;
 
@@ -153,12 +147,14 @@ void sendMessage(int device, int channel, int byte0, int byte1, int byte2)
 {
     auto midiOutputDevices = juce::MidiOutput::getAvailableDevices();
 
+    // check device
     if (midiOutputDevices.isEmpty())
     {
         std::cerr << "ERROR: No MIDI output devices found." << std::endl;
         exit(1);
     }
 
+    // select device
     auto deviceInfo = midiOutputDevices[device - 1];
     auto midiOutput = juce::MidiOutput::openDevice(deviceInfo.identifier);
     if (midiOutput == nullptr)
@@ -167,41 +163,20 @@ void sendMessage(int device, int channel, int byte0, int byte1, int byte2)
         exit(1);
     }
 
+    // show dump
     std::stringstream hex0, hex1, hex2;
     hex0 << std::hex << std::uppercase << byte0 << "h";
     hex1 << std::hex << std::uppercase << byte1 << "h";
     hex2 << std::hex << std::uppercase << byte2 << "h";
-
     std::cout << "  device : " << device << " " << deviceInfo.name << std::endl;
     std::cout << "  channel: " << channel << std::endl;
     std::cout << "  bytes  : " << hex0.str() << " " << hex1.str() << " " << hex2.str();
     std::cout << " (" << byte0 << " " << byte1 << " " << byte2 << ")" << std::endl;
+    std::cout << "  " << toString(byte0, byte1, byte2) << std::endl;
 
-    int msgtype = byte0 & 0xF0;
-
-    if (msgtype == 0x90)      // Note On
-    {
-        auto msg = juce::MidiMessage::noteOn(channel, byte1, (juce::uint8)byte2);
-        midiOutput->sendMessageNow(msg);
-    }
-    else if (msgtype == 0x80) // Note Off
-    {
-        auto msg = juce::MidiMessage::noteOff(channel, byte1, (juce::uint8)byte2);
-        midiOutput->sendMessageNow(msg);
-    }
-    else if (msgtype == 0xB0) // CC
-    {
-        if (byte1 == 120) // All Sound Off
-        {
-            auto msg = juce::MidiMessage::allSoundOff(channel);
-            midiOutput->sendMessageNow(msg);
-        }
-        else if (byte1 == 123) // All Notes Off
-        {
-            auto msg = juce::MidiMessage::allNotesOff(channel);
-            midiOutput->sendMessageNow(msg);
-        }
-    }
+    // send message
+    auto msg = juce::MidiMessage(byte0, byte1, byte2);
+    midiOutput->sendMessageNow(msg);
 }
 
 
@@ -256,5 +231,23 @@ int getNumber(std::string s)
     }
 
     return value;
+}
+
+
+std::string toString(int byte0, int byte1, int byte2)
+{
+    std::string s = "";
+
+    int cmd = byte0 & 0xF0;
+    if      (cmd == 0x90) { s = "[NoteOn]  note number:" + std::to_string(byte1) + " velocity:" + std::to_string(byte2); }
+    else if (cmd == 0x80) { s = "[NoteOff] note number:" + std::to_string(byte1) + " velocity:" + std::to_string(byte2); }
+
+    else if (cmd == 0xB0) {
+        int cc_cmd = byte1;
+        if      (cc_cmd == 0x78) { s = "[CC] [All Sound Off] byte2(to be zero):" + std::to_string(byte2); }
+        else if (cc_cmd == 0x7B) { s = "[CC] [All Notes Off] byte2(to be zero):" + std::to_string(byte2); }
+    }
+
+    return s;
 }
 
